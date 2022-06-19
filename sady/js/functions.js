70fba47703274939a85ce2ab6ee459d2c67578b1
gokiimax <schaefer.maxi09101@gmail.com>
2022-06-19 19:57:26 +0200

    Updated Settings

--- sady/js/functions.js
+++ sady/js/functions.js
@@ -24,14 +24,16 @@ let isLeftMenuActive = false
 
 settingsButton.addEventListener('click', () => {
     if(!isLeftMenuActive) {
-        Sidebar.style.width = '240px';
+        Sidebar.style.width = '300px';
         settingsForm.style.opacity = "1";
 
+        settingsButton.classList.add("clicked")
         isLeftMenuActive = true;
     } else {
         Sidebar.style.width = '0px';
         settingsForm.style.opacity = "0";
 
+        settingsButton.classList.remove("clicked")
         isLeftMenuActive = false;
     }
 })
@@ -97,6 +99,7 @@ if(window.localStorage.getItem("heading")) {
 
 if(window.localStorage.getItem("background")) {
     document.body.style.background = `url("${window.localStorage.getItem("background")}")`
+    backgroundLink.value = window.localStorage.getItem("background");
 }
 
 if(window.localStorage.getItem("headerColor")) {
@@ -113,4 +116,42 @@ if(window.localStorage.getItem("bookmarksColor")) {
     bookmarks.getElementsByTagName("a")[2].style.color = window.localStorage.getItem("bookmarksColor");
     bookmarks.getElementsByTagName("a")[3].style.color = window.localStorage.getItem("bookmarksColor");
     bookmarks.getElementsByTagName("a")[4].style.color = window.localStorage.getItem("bookmarksColor");
+}
+
+/* ============================== Reset ============================== */
+
+const resetAll = document.getElementById("resetAll")
+const resetColors = document.getElementById("resetColors")
+
+resetAll.addEventListener("click", () => {
+    window.localStorage.clear();
+    location.reload();
+})
+
+resetColors.addEventListener("click", () => {
+    window.localStorage.removeItem("bookmarksColor");
+    window.localStorage.removeItem("clockColor");
+    window.localStorage.removeItem("headerColor");
+    window.localStorage.removeItem("glowColor");
+    location.reload();
+})
+
+/* ============================== Glow ============================== */
+const glowColor = document.querySelectorAll(".glow-font")
+const glow = document.getElementById("glow")
+
+glow.addEventListener("keypress", (e) => {
+    if(glow.value) {
+        glowColor.forEach(glow1 => {
+            glow1.style.textShadow = `0 5px 15px ${glow.value}`
+        })
+
+        window.localStorage.setItem("glowColor", glow.value)
+    }
+})
+
+if(window.localStorage.getItem("glowColor")) {
+    glowColor.forEach(glow1 => {
+        glow1.style.textShadow = `0 5px 15px ${window.localStorage.getItem("glowColor")}`
+    })
 }
